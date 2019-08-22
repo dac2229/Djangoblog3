6b111a6124a003974720414306f2af7820fbf4c3
dac2229 <dac2229@gmail.com>
2019-08-21 22:53:56 -0700

    added categories and added string format output

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,6 @@
-# a new import
 from django.contrib import admin
-from blogging.models import Post
+from blogging.models import Post, Category  # <-- import Category
 
-# and a new admin registration
+
 admin.site.register(Post)
+admin.site.register(Category)               # <-- Register Category
--- blogging/models.py
+++ blogging/models.py
@@ -13,6 +13,10 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+    def __str__(self):
+        return self.name
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
     posts = models.ManyToManyField(Post, blank=True, related_name='categories')
